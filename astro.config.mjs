2cbd2c89085b2ba6297ba6b33e61b0428bb9178c
Kevin Ybarra <kevin_link_23@hotmail.com>
2025-11-08 18:50:07 -0300

    Project initial state

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    server: {
+        allowedHosts: true
+    }
+});
