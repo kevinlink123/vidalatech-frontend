ec82305d36b98b6add3388a52430a12da7ecf064
Kevinlink123 <kevin_link_23@hotmail.com>
2026-05-21 14:32:16 -0300

    feat(app): Added sitemap integration to generate the sitemap's xml files at build time

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    },
-    server: {
-        allowedHosts: true
-    }
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  server: {
+      allowedHosts: true
+  },
+  site: "https://vidalatech.dpdns.org/",
+  integrations: [sitemap()]
 });
